4142c30d8d2ce61c9e9fe83d3ccbc27deabefcb8
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-04 17:08:17 -0500

    Done.

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,22 @@
 import React, { Fragment } from 'react'
 
 const Sushi = (props) => {
+  const {sushi, eaten, addEatenSushi} = props
+  const {img_url, name, price} = sushi
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
+           onClick={() => addEatenSushi(sushi)}>
         { 
           /* Tell me if this sushi has been eaten! */ 
-          false ?
+          eaten ?
             null
           :
-            <img src={/* Give me an image source! */ } width="100%" />
+            <img src={img_url} width="100%" />
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {name} - ${price}
       </h4>
     </div>
   )
